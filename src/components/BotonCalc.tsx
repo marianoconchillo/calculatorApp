18cb5440ab8d4db70e79c82e4b38df109b951e38
marianoconchillo <marianoconchillo@hotmail.com>
2022-03-23 15:29:11 -0300

    First commit

---
+++ src/components/BotonCalc.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface Props {
+    texto: string,
+    color?: string,
+    ancho?: boolean,
+    accion: (numeroTexto: string) => void,
+}
+
+export const BotonCalc = ({ texto, color = "#2D2D2D", ancho, accion }: Props) => {
+    return (
+        <TouchableOpacity
+            onPress={() => accion(texto)}>
+            <View style={{
+                ...styles.boton,
+                backgroundColor: color,
+                width: (ancho) ? 180 : 80
+            }}>
+                <Text style={{
+                    ...styles.botonTexto,
+                    color: (color === "#9B9B9B") ? "black" : "white",
+                }}>{texto}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    boton: {
+        height: 80,
+        width: 80,
+        borderRadius: 100,
+        justifyContent: "center",
+        marginHorizontal: 5
+    },
+    botonTexto: {
+        textAlign: "center",
+        padding: 10,
+        fontSize: 30,
+        color: "white",
+        fontWeight: "400"
+    }
+});
